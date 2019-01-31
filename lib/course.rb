e002924f2d506d692402688c067dcf35e1d11cd5
Michael Soares <mikey.soares@gmail.com>
2019-01-31 17:38:47 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,5 +1,6 @@
 class Course
   attr_accessor :title, :schedule, :description
+<<<<<<< HEAD
 
 @@all = []
   def initialize
@@ -15,4 +16,15 @@ class Course
 
   end
 
+=======
+@@all = []
+  def initialize
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+>>>>>>> 629d821b459548d0056851b5f9456b0efcd08125
 end
